a71db18c599926a595412351c7d389aa6f7591af
wwwqsid <132813941+wwwqsid@users.noreply.github.com>
2023-11-03 22:18:15 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
 ﻿//******<<>>（https://www.wyeditor.com，免费软件欢迎下载使用） JS库文件*******
- $(function(){ 
- $('.tabPanelTabsDiv1 ul li').click(function(){ 
- $(this).addClass('hit').siblings().removeClass('hit'); 
- $('.panesTabsDiv1>div:eq('+$(this).index()+')').show().siblings().hide(); 
- }) 
- })
+var clickTimeId;
+function img_744353_click(o)
+{
+ var aurl=$("img[name=img_744353_img]").attr('src'); if(aurl!=undefined) {window.open(aurl);}
+
+}
+function img_326664_click(o)
+{
+ var aurl=$("img[name=img_326664_img]").attr('src'); if(aurl!=undefined) {window.open(aurl);}
+
+}
+function img_58619_click(o)
+{
+ var aurl=$("img[name=img_58619_img]").attr('src'); if(aurl!=undefined) {window.open(aurl);}
+
+}
+ $(document).ready(function(){ 
+ $('.flexslider').flexslider({ 
+ directionNav: true, 
+pauseOnAction: false,
+pauseOnHover: false,
+controlNav: true,
+animationLoop: true,
+ keyboardNav: true, 
+ mousewheel: true, 
+ slideshowSpeed: 5000, 
+ animationSpeed: 1600
+  
+ }); 
+ });
--- js/indexaft.js
+++ js/indexaft.js
@@ -8,4 +8,26 @@ var qrcodeQrCode_378150 = new QRCode(document.getElementById("QrCode_378150"), {
  qrcodeQrCode_378150.makeCode('https://wwwqsid.github.io/'); 
  } 
   
- makeCodeQrCode_378150();
+ makeCodeQrCode_378150(); 
+
+       var imgs = document.querySelectorAll('img'); 
+         function getTop(e) { 
+             var T = e.offsetTop; 
+             while(e = e.offsetParent) { 
+                 T += e.offsetTop; 
+             } 
+             return T; 
+         } 
+         function lazyLoad(imgs) { 
+             var H = document.documentElement.clientHeight;//获取可视区域高度 
+             var S = document.documentElement.scrollTop || document.body.scrollTop; 
+             for (var i = 0; i < imgs.length; i++) { 
+                 if (H + S > getTop(imgs[i])) { 
+                     var myscr = imgs[i].getAttribute('data-src');
+                    if((String(myscr).length != 0)&&(String(imgs[i].src).length == 0 )){ imgs[i].src = myscr;} 
+                 } 
+             } 
+         } 
+         window.onload = window.onscroll = function () { //onscroll()在滚动条滚动的时候触发 
+             lazyLoad(imgs); 
+         }
