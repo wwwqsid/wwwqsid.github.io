fe0781f5778e900e9fe8678c459c448e2e41bb19
wwwqsid <132813941+wwwqsid@users.noreply.github.com>
2023-11-03 21:48:44 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+﻿//******<<>>（https://www.wyeditor.com，免费软件欢迎下载使用） JS库文件*******
+ $(function(){ 
+ $('.tabPanelTabsDiv1 ul li').click(function(){ 
+ $(this).addClass('hit').siblings().removeClass('hit'); 
+ $('.panesTabsDiv1>div:eq('+$(this).index()+')').show().siblings().hide(); 
+ }) 
+ })
