fce2d3d2e439b1d11e84cc455b51ab424a230068
wwwqsid <132813941+wwwqsid@users.noreply.github.com>
2023-11-03 21:36:08 +0800

    Delete js directory

--- js/1.js
+++
@@ -1,7 +0,0 @@
-﻿//******<<>>（https://www.wyeditor.com，免费软件欢迎下载使用） JS库文件*******
- $(function(){ 
- $('.tabPanelTabsDiv1 ul li').click(function(){ 
- $(this).addClass('hit').siblings().removeClass('hit'); 
- $('.panesTabsDiv1>div:eq('+$(this).index()+')').show().siblings().hide(); 
- }) 
- })
